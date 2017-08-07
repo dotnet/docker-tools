a34aa353175361a73410e619587db72885dbe46b
MichaelSimons <msimons@microsoft.com>
2017-08-07 13:46:45 -0500

    Upgrade manifest-tool version and make use of tags element

--- src/Microsoft.DotNet.ImageBuilder/Commands/PublishManifestCommand.cs
+++ src/Microsoft.DotNet.ImageBuilder/Commands/PublishManifestCommand.cs
@@ -5,6 +5,7 @@
 using Microsoft.DotNet.ImageBuilder.Model;
 using Microsoft.DotNet.ImageBuilder.ViewModel;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,40 +21,45 @@ namespace Microsoft.DotNet.ImageBuilder.Commands
         public override void Execute()
         {
             WriteHeading("GENERATING MANIFESTS");
-            foreach (RepoInfo repo in Manifest.Repos)
+            IEnumerable<ImageInfo> multiArchImages = Manifest.Repos
+                .SelectMany(repo => repo.Images)
+                .Where(image => image.SharedTags.Any());
+            foreach (ImageInfo image in multiArchImages)
             {
-                foreach (ImageInfo image in repo.Images)
+                StringBuilder manifestYml = new StringBuilder();
+                manifestYml.AppendLine($"image: {image.SharedTags.First().FullyQualifiedName}");
+
+                IEnumerable<string> additionalTags = image.SharedTags
+                    .Select(tag => tag.Name)
+                    .Skip(1);
+                if (additionalTags.Any())
                 {
-                    foreach (TagInfo tag in image.SharedTags)
+                    manifestYml.AppendLine($"tags: [{string.Join(",", additionalTags)}]");
+                }
+
+                manifestYml.AppendLine("manifests:");
+                foreach (PlatformInfo platform in image.Platforms)
+                {
+                    manifestYml.AppendLine($"  -");
+                    manifestYml.AppendLine($"    image: {platform.Tags.First().FullyQualifiedName}");
+                    manifestYml.AppendLine($"    platform:");
+                    manifestYml.AppendLine($"      architecture: {platform.Model.Architecture.ToString().ToLowerInvariant()}");
+                    manifestYml.AppendLine($"      os: {platform.Model.OS}");
+                    if (platform.Model.Variant != null)
                     {
-                        StringBuilder manifestYml = new StringBuilder();
-                        manifestYml.AppendLine($"image: {tag.FullyQualifiedName}");
-                        manifestYml.AppendLine("manifests:");
-
-                        foreach (PlatformInfo platform in image.Platforms)
-                        {
-                            manifestYml.AppendLine($"  -");
-                            manifestYml.AppendLine($"    image: {platform.Tags.First().FullyQualifiedName}");
-                            manifestYml.AppendLine($"    platform:");
-                            manifestYml.AppendLine($"      architecture: {platform.Model.Architecture.ToString().ToLowerInvariant()}");
-                            manifestYml.AppendLine($"      os: {platform.Model.OS}");
-                            if (platform.Model.Variant != null)
-                            {
-                                manifestYml.AppendLine($"      variant: {platform.Model.Variant}");
-                            }
-                        }
-
-                        Console.WriteLine($"-- PUBLISHING MANIFEST:{Environment.NewLine}{manifestYml}");
-                        File.WriteAllText("manifest.yml", manifestYml.ToString());
-
-                        // ExecuteWithRetry because the manifest-tool fails periodically with communicating
-                        // with the Docker Registry.
-                        ExecuteHelper.ExecuteWithRetry(
-                            "manifest-tool",
-                            $"--username {Options.Username} --password {Options.Password} push from-spec manifest.yml",
-                            Options.IsDryRun);
+                        manifestYml.AppendLine($"      variant: {platform.Model.Variant}");
                     }
                 }
+
+                Console.WriteLine($"-- PUBLISHING MANIFEST:{Environment.NewLine}{manifestYml}");
+                File.WriteAllText("manifest.yml", manifestYml.ToString());
+
+                // ExecuteWithRetry because the manifest-tool fails periodically with communicating
+                // with the Docker Registry.
+                ExecuteHelper.ExecuteWithRetry(
+                    "manifest-tool",
+                    $"--username {Options.Username} --password {Options.Password} push from-spec manifest.yml",
+                    Options.IsDryRun);
             }
         }
     }

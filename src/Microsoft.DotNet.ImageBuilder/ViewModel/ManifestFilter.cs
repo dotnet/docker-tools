9fa57d38ed7bff97f362aa097f195f225d683556
Ravi Eda <raeda@microsoft.com>
2017-10-04 15:05:03 -0500

    Add regex support on path argument in ImageBuilder (#49)
    * Add regex support on path.
    
    Remove and sort usings.
    
    Add support for wildcard characters.

--- src/Microsoft.DotNet.ImageBuilder/ViewModel/ManifestFilter.cs
+++ src/Microsoft.DotNet.ImageBuilder/ViewModel/ManifestFilter.cs
@@ -5,6 +5,7 @@
 using Microsoft.DotNet.ImageBuilder.Model;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Microsoft.DotNet.ImageBuilder.ViewModel
 {
@@ -28,8 +29,12 @@ namespace Microsoft.DotNet.ImageBuilder.ViewModel
 
         public IEnumerable<Platform> GetPlatforms(Image image)
         {
+            bool isPathBlank = string.IsNullOrWhiteSpace(IncludePath);
+            string pattern = isPathBlank ? 
+                null : "^" + Regex.Escape(IncludePath).Replace(@"\*", ".*").Replace(@"\?", ".");
+
             return image.Platforms
-                .Where(platform => string.IsNullOrWhiteSpace(IncludePath) || platform.Dockerfile.StartsWith(IncludePath));
+                .Where(platform => isPathBlank || Regex.IsMatch(platform.Dockerfile, pattern, RegexOptions.IgnoreCase));
         }
 
         public IEnumerable<Repo> GetRepos(Manifest manifest)

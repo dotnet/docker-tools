1e91baa689de78246397cb0d2372a70ba11f709f
MichaelSimons <msimons@microsoft.com>
2017-11-22 14:00:15 -0600

    Refactor folder layout for utests

---
+++ Microsoft.DotNet.ImageBuilder/tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Microsoft.DotNet.ImageBuilder.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

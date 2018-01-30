4d5dcae03d1f7efef00eeb298df5ec9ee4f22175
Michael Simons <msimons@microsoft.com>
2018-01-30 08:38:18 -0600

    Enable CI (#84)

---
+++ netci.groovy
@@ -0,0 +1,32 @@
+import jobs.generation.Utilities
+
+def project = GithubProject
+def branch = GithubBranchName
+def isPR = true
+def platformList = ['Debian', 'NanoServer']
+
+platformList.each { platform ->
+    def newJobName = Utilities.getFullJobName(project, platform, isPR)
+
+    def newJob = job(newJobName) {
+        steps {
+            if (platform == 'NanoServer') {
+                batchFile("powershell -NoProfile -File .\\Microsoft.DotNet.ImageBuilder\\build.ps1 -CleanupDocker")
+            }
+            else {
+                shell("docker build -t runner -f ./Microsoft.DotNet.ImageBuilder/Dockerfile.linux.runner .")
+                shell("docker run -v /var/run/docker.sock:/var/run/docker.sock runner pwsh -File ./Microsoft.DotNet.ImageBuilder/build.ps1 -CleanupDocker")
+            }
+        }
+    }
+
+    if (platform == 'NanoServer') {
+        newJob.with {label('windows.10.amd64.serverrs3.open')}
+    }
+    else {
+        Utilities.setMachineAffinity(newJob, 'Ubuntu16.04', 'latest-or-auto-docker')
+    }
+
+    Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+    Utilities.addGithubPRTriggerForBranch(newJob, branch, platform)
+}

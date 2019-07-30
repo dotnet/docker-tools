4914027d75d2b1a76c1346017934ef3692f5d1fe
Matt Thalman <mthalman@microsoft.com>
2019-07-30 13:26:55 -0500

    Include simple tags when merging image info (#234)
    Ensures that simple tags gets merged appropriately when merging image info data. I've generalized the algorithm to use reflection to ensure that every property is accounted for to prevent issues like this in the future. Updates unit tests to verify the scenario.

--- Microsoft.DotNet.ImageBuilder/src/ImageInfoHelper.cs
+++ Microsoft.DotNet.ImageBuilder/src/ImageInfoHelper.cs
@@ -2,8 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Microsoft.DotNet.ImageBuilder.Models.Image;
 
 namespace Microsoft.DotNet.ImageBuilder
@@ -21,53 +24,105 @@ namespace Microsoft.DotNet.ImageBuilder
                 }
                 else
                 {
-                    MergeImages(srcRepo, targetRepo);
+                    MergeData(srcRepo, targetRepo);
                 }
             }
         }
 
-        private static void MergeImages(RepoData srcRepo, RepoData targetRepo)
+        private static void MergeData(object srcObj, object targetObj)
         {
-            if (srcRepo.Images == null)
+            if (srcObj.GetType() != targetObj.GetType())
             {
-                return;
+                throw new ArgumentException("Object types don't match.", nameof(targetObj));
             }
 
-            if (srcRepo.Images.Any() && targetRepo.Images == null)
+            foreach (PropertyInfo property in srcObj.GetType().GetProperties())
             {
-                targetRepo.Images = srcRepo.Images;
-                return;
-            }
-
-            foreach (KeyValuePair<string, ImageData> srcKvp in srcRepo.Images)
-            {
-                if (targetRepo.Images.TryGetValue(srcKvp.Key, out ImageData targetImage))
+                if (property.PropertyType == typeof(string))
                 {
-                    MergeDigests(srcKvp.Value, targetImage);
+                    property.SetValue(targetObj, property.GetValue(srcObj));
+                }
+                else if (typeof(IDictionary).IsAssignableFrom(property.PropertyType))
+                {
+                    MergeDictionaries(property, srcObj, targetObj);
+                }
+                else if (typeof(IList<string>).IsAssignableFrom(property.PropertyType))
+                {
+                    MergeLists(property, srcObj, targetObj);
                 }
                 else
                 {
-                    targetRepo.Images.Add(srcKvp.Key, srcKvp.Value);
+                    throw new NotSupportedException($"Unsupported model property type: '{property.PropertyType.FullName}'");
                 }
             }
         }
 
-        private static void MergeDigests(ImageData srcImage, ImageData targetImage)
+        private static void MergeLists(PropertyInfo property, object srcObj, object targetObj)
         {
-            if (srcImage.BaseImages == null)
+            IList<string> srcList = (IList<string>)property.GetValue(srcObj);
+            if (srcList == null)
             {
                 return;
             }
 
-            if (srcImage.BaseImages.Any() && targetImage.BaseImages == null)
+            IList<string> targetList = (IList<string>)property.GetValue(targetObj);
+
+            if (srcList.Any())
             {
-                targetImage.BaseImages = srcImage.BaseImages;
+                if (targetList != null)
+                {
+                    targetList = targetList
+                        .Union(srcList)
+                        .OrderBy(element => element)
+                        .ToList();
+                }
+                else
+                {
+                    targetList = srcList;
+                }
+
+                property.SetValue(targetObj, targetList);
+            }
+        }
+
+        private static void MergeDictionaries(PropertyInfo property, object srcObj, object targetObj)
+        {
+            IDictionary srcDict = (IDictionary)property.GetValue(srcObj);
+            if (srcDict == null)
+            {
                 return;
             }
 
-            foreach (KeyValuePair<string, string> srcKvp in srcImage.BaseImages)
+            IDictionary targetDict = (IDictionary)property.GetValue(targetObj);
+
+            if (srcDict.Cast<object>().Any())
             {
-                targetImage.BaseImages[srcKvp.Key] = srcKvp.Value;
+                if (targetDict != null)
+                {
+                    foreach (dynamic kvp in srcDict)
+                    {
+                        if (targetDict.Contains(kvp.Key))
+                        {
+                            object newValue = kvp.Value;
+                            if (newValue is string)
+                            {
+                                targetDict[kvp.Key] = newValue;
+                            }
+                            else
+                            {
+                                MergeData(kvp.Value, targetDict[kvp.Key]);
+                            }
+                        }
+                        else
+                        {
+                            targetDict[kvp.Key] = kvp.Value;
+                        }
+                    }
+                }
+                else
+                {
+                    property.SetValue(targetObj, srcDict);
+                }
             }
         }
     }

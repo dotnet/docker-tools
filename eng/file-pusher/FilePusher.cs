dd8252af1f837b55e23237c97a87406c694e0879
MichaelSimons <msimons@microsoft.com>
2019-08-09 15:36:52 -0500

    Implement utility to push eng/common to product repos

---
+++ eng/file-pusher/FilePusher.cs
@@ -0,0 +1,214 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using FilePusher.Models;
+using Microsoft.DotNet.VersionTools.Automation;
+using Microsoft.DotNet.VersionTools.Automation.GitHubApi;
+using Newtonsoft.Json;
+
+namespace FilePusher
+{
+    public class FilePusher
+    {
+        private static Options Options { get; } = new Options();
+
+        public static async Task Main(string[] args)
+        {
+            // TODO:  Add support for delete file scenarios
+
+            // Hookup a TraceListener to capture details from Microsoft.DotNet.VersionTools
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+
+            Options.Parse(args);
+
+            string subscriptionsJson = File.ReadAllText(Options.SubscriptionsPath);
+            Subscriptions subscriptions = JsonConvert.DeserializeObject<Subscriptions>(subscriptionsJson);
+
+            foreach (GitRepo repo in GetFilteredRepos(subscriptions))
+            {
+                Console.WriteLine($"Processing {repo.Name}/{repo.Branch}");
+
+                await ExecuteGitOperationsWithRetryAsync(Options, async client =>
+                {
+                    await CreatePullRequestAsync(client, repo, subscriptions);
+                });
+            }
+        }
+
+        private async static Task AddUpdatedFile(
+            List<GitObject> updatedFiles,
+            GitHubClient client,
+            GitHubBranch branch,
+            string filePath,
+            string updatedContent)
+        {
+            if (updatedContent.Contains("\r\n"))
+            {
+                updatedContent = updatedContent.Replace("\r\n", "\n");
+            }
+
+            string currentContent = await client.GetGitHubFileContentsAsync(filePath, branch);
+
+            if (currentContent == updatedContent)
+            {
+                Console.WriteLine($"File '{filePath}' has not changed.");
+            }
+            else
+            {
+                Console.WriteLine($"File '{filePath}' has changed.");
+                updatedFiles.Add(new GitObject
+                {
+                    Path = filePath.Replace('\\','/'),
+                    Type = GitObject.TypeBlob,
+                    Mode = GitObject.ModeFile,
+                    Content = updatedContent
+                });
+            }
+        }
+
+        private async static Task<bool> BranchExists(GitHubClient client, GitHubProject project, string @ref)
+        {
+            try
+            {
+                await client.GetReferenceAsync(project, @ref);
+                return true;
+            }
+            catch (HttpFailureResponseException)
+            {
+                return false;
+            }
+        }
+
+        private static IEnumerable<GitRepo> GetFilteredRepos(Subscriptions subscriptions)
+        {
+            IEnumerable<GitRepo> activeRepos = subscriptions.Repos;
+            if (Options.Filters?.Any() ?? false)
+            {
+                string pathsRegexPattern = GetFilterRegexPattern(Options.Filters.ToArray());
+                activeRepos = activeRepos.Where(repo =>
+                    Regex.IsMatch(repo.ToString(), pathsRegexPattern, RegexOptions.IgnoreCase));
+            }
+
+            if (!activeRepos.Any())
+            {
+                Console.WriteLine("No repos found to update.");
+                Environment.Exit(1);
+            }
+
+            return activeRepos;
+        }
+
+        private static async Task CreatePullRequestAsync(
+            GitHubClient client,
+            GitRepo gitRepo,
+            Subscriptions subscriptions)
+        {
+            GitHubProject project = new GitHubProject(gitRepo.Name, gitRepo.Owner);
+            GitHubProject forkedProject = new GitHubProject(gitRepo.Name, Options.GitUser);
+            GitHubBranch baseBranch = new GitHubBranch(gitRepo.Branch, project);
+            GitHubBranch headBranch = new GitHubBranch($"{gitRepo.Owner}{subscriptions.WorkingBranchSuffix}", forkedProject);
+
+            IEnumerable<GitObject> changes = await GetUpdatedFiles(subscriptions.SourcePath, client, baseBranch);
+
+            if (!changes.Any())
+            {
+                return;
+            }
+
+            GitReference currentRef = await client.GetReferenceAsync(project, $"heads/{baseBranch.Name}");
+            string parentSha = currentRef.Object.Sha;
+            GitTree tree = await client.PostTreeAsync(forkedProject, parentSha, changes.ToArray());
+            GitCommit commit = await client.PostCommitAsync(
+                forkedProject,
+                subscriptions.CommitMessage,
+                tree.Sha,
+                new[] { parentSha });
+
+            string workingReference = $"heads/{headBranch.Name}";
+            if (await BranchExists(client, forkedProject, workingReference))
+            {
+                await client.PatchReferenceAsync(forkedProject, workingReference, commit.Sha, force: true);
+            }
+            else
+            {
+                await client.PostReferenceAsync(forkedProject, workingReference, commit.Sha);
+            }
+
+            GitHubPullRequest pullRequestToUpdate = await client.SearchPullRequestsAsync(
+                project,
+                headBranch.Name,
+                await client.GetMyAuthorIdAsync());
+
+            if (pullRequestToUpdate == null)
+            {
+                await client.PostGitHubPullRequestAsync(
+                    subscriptions.PullRequestTitle,
+                    subscriptions.PullRequestDescription,
+                    headBranch,
+                    baseBranch,
+                    maintainersCanModify: true);
+            }
+        }
+
+        public static async Task ExecuteGitOperationsWithRetryAsync(
+            Options options,
+            Func<GitHubClient, Task> execute,
+            int maxTries = 10,
+            int retryMillisecondsDelay = 5000)
+        {
+            GitHubAuth githubAuth = new GitHubAuth(options.GitAuthToken, options.GitUser, options.GitEmail);
+            using (GitHubClient client = new GitHubClient(githubAuth))
+            {
+                for (int i = 0; i < maxTries; i++)
+                {
+                    try
+                    {
+                        await execute(client);
+
+                        break;
+                    }
+                    catch (HttpRequestException ex) when (i < (maxTries - 1))
+                    {
+                        Console.WriteLine($"Encountered exception interacting with GitHub: {ex.Message}");
+                        Console.WriteLine($"Trying again in {retryMillisecondsDelay}ms. {maxTries - i - 1} tries left.");
+                        await Task.Delay(retryMillisecondsDelay);
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<string> GetFiles(string targetDirectory) =>
+            Directory.GetDirectories(targetDirectory)
+                .SelectMany(dir => GetFiles(dir))
+                .Concat(Directory.GetFiles(targetDirectory));
+
+        private static string GetFilterRegexPattern(params string[] patterns)
+        {
+            string processedPatterns = patterns
+                .Select(pattern => Regex.Escape(pattern).Replace(@"\*", ".*").Replace(@"\?", "."))
+                .Aggregate((working, next) => $"{working}|{next}");
+            return $"^({processedPatterns})$";
+        }
+
+        private async static Task<GitObject[]> GetUpdatedFiles(string sourcePath, GitHubClient client, GitHubBranch branch)
+        {
+            List<GitObject> updatedFiles = new List<GitObject>();
+
+            foreach (string file in GetFiles(sourcePath))
+            {
+                await AddUpdatedFile(updatedFiles, client, branch, file, File.ReadAllText(file));
+            }
+
+            return updatedFiles.ToArray();
+        }
+    }
+}
---
+++ eng/file-pusher/Options.cs
@@ -0,0 +1,69 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.CommandLine;
+
+namespace FilePusher
+{
+    public class Options
+    {
+        public IEnumerable<string> Filters { get; private set; }
+        public string GitEmail { get; private set; }
+        public string GitAuthToken { get; private set; }
+        public string GitUser { get; private set; }
+        public string SubscriptionsPath { get; private set; }
+
+        public void Parse(string[] args)
+        {
+            ArgumentSyntax argSyntax = ArgumentSyntax.Parse(args, syntax =>
+            {
+                IReadOnlyList<string> filters = Array.Empty<string>();
+                syntax.DefineOptionList(
+                    "filter",
+                    ref filters,
+                    "Filter to apply to repositories of the subscriptions.json - wildcard chars * and ? supported");
+                Filters = filters;
+
+                string subscriptionsPath = null;
+                syntax.DefineParameter(
+                    "subscriptions",
+                    ref subscriptionsPath,
+                    "Path to the subscritions json file (defaults to subscriptions.json)");
+                SubscriptionsPath = subscriptionsPath;
+
+                string gitUser = null;
+                syntax.DefineParameter(
+                    "git-user",
+                    ref gitUser,
+                    "GitHub user used to make PR");
+                GitUser = gitUser;
+
+                string gitEmail = null;
+                syntax.DefineParameter(
+                    "git-email",
+                    ref gitEmail,
+                    "GitHub email used to make PR");
+                GitEmail = gitEmail;
+
+                string gitAuthToken = null;
+                syntax.DefineParameter(
+                    "git-auth-token",
+                    ref gitAuthToken,
+                    "GitHub authorization token used to make PR");
+                GitAuthToken = gitAuthToken;
+            });
+
+            // Workaround for https://github.com/dotnet/corefxlab/issues/1689
+            foreach (Argument arg in argSyntax.GetActiveArguments())
+            {
+                if (arg.IsParameter && !arg.IsSpecified)
+                {
+                    Console.WriteLine($"error: `{arg.Name}` must be specified.");
+                    Environment.Exit(1);
+                }
+            }
+        }
+    }
+}

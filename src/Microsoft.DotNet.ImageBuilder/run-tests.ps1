9ddf6f77283b494a4fbab73c8376acfbd8e235be
Matt Thalman <mthalman@microsoft.com>
2020-06-24 16:35:00 -0500

    Add test script for Image Builder (#567)

---
+++ src/Microsoft.DotNet.ImageBuilder/run-tests.ps1
@@ -0,0 +1,27 @@
+#!/usr/bin/env pwsh
+#
+# Copyright (c) .NET Foundation and contributors. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$dotnetInstallDir = "$PSScriptRoot/../../.dotnet"
+
+Push-Location $PSScriptRoot
+
+try {
+    & ../../eng/common/Install-DotNetSdk.ps1 $dotnetInstallDir
+
+    $cmd = "$DotnetInstallDir/dotnet test --logger:trx"
+
+    Write-Output "Executing '$cmd'"
+    Invoke-Expression $cmd
+    if ($LASTEXITCODE -ne 0) {
+        throw "Failed: '$cmd'"
+    }
+}
+finally {
+    Pop-Location
+}

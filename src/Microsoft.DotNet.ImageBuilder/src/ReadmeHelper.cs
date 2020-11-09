d18cd6b5537192a0c755685ba51d9c6524e37942
Matt Thalman <mthalman@microsoft.com>
2020-11-09 10:18:57 -0600

    Update to look for a marker that indicates end of generated tags

--- src/Microsoft.DotNet.ImageBuilder/src/ReadmeHelper.cs
+++ src/Microsoft.DotNet.ImageBuilder/src/ReadmeHelper.cs
@@ -2,13 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Text.RegularExpressions;
+using Sprache;
 
 namespace Microsoft.DotNet.ImageBuilder
 {
     public static class ReadmeHelper
     {
         private const string TagsSectionHeader = "# Full Tag Listing";
+        private const string EndOfGeneratedTagsMarker = "<!--End of generated tags-->";
 
         public static string UpdateTagsListing(string readme, string tagsListing)
         {
@@ -16,11 +17,22 @@ namespace Microsoft.DotNet.ImageBuilder
             tagsListing = tagsListing.NormalizeLineEndings(readme);
 
             string targetLineEnding = readme.GetLineEndingFormat();
-            tagsListing = $"{TagsSectionHeader}{targetLineEnding}{targetLineEnding}{tagsListing}{targetLineEnding}";
 
-            // Regex to find the entire tags listing section including the header.
-            Regex regex = new Regex($"^{TagsSectionHeader}\\s*", RegexOptions.Multiline);
-            return regex.Replace(readme, tagsListing);
+            Parser<string> parser =
+                from leadingContent in Parse.AnyChar.Until(Parse.String(TagsSectionHeader + targetLineEnding)).Text()
+                from tagsContent in Parse.AnyChar.Until(Parse.String(EndOfGeneratedTagsMarker + targetLineEnding)).Text()
+                from trailingContent in Parse.AnyChar.Many().Text()
+                select string.Concat(
+                    leadingContent,
+                    TagsSectionHeader,
+                    targetLineEnding,
+                    targetLineEnding,
+                    tagsListing,
+                    EndOfGeneratedTagsMarker,
+                    targetLineEnding,
+                    trailingContent);
+
+            return parser.Parse(readme);
         }
     }
 }

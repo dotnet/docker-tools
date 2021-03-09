6d4d43305d2c482cdebe40aee85ca8643a5f19d7
Michael Simons <msimons@microsoft.com>
2021-03-09 13:20:40 -0600

    Rename GetImageLayers to GetImageManifestLayers

--- src/Microsoft.DotNet.ImageBuilder/src/Commands/BuildCommand.cs
+++ src/Microsoft.DotNet.ImageBuilder/src/Commands/BuildCommand.cs
@@ -183,7 +183,7 @@ namespace Microsoft.DotNet.ImageBuilder.Commands
         {
             if (platform.Layers == null || !platform.Layers.Any())
             {
-                platform.Layers = _dockerService.GetImageLayers(tag, Options.IsDryRun).ToList();
+                platform.Layers = _dockerService.GetImageManifestLayers(tag, Options.IsDryRun).ToList();
             }
         }
 

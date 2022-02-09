886b315673e9b4f5ecf0b0032ee4dd046ff234cc
Matt Thalman <mthalman@microsoft.com>
2022-02-09 10:13:36 -0600

    Use image info as input to pullImages command (#965)

--- src/Microsoft.DotNet.ImageBuilder/src/Commands/PullImagesOptions.cs
+++ src/Microsoft.DotNet.ImageBuilder/src/Commands/PullImagesOptions.cs
@@ -15,6 +15,7 @@ namespace Microsoft.DotNet.ImageBuilder.Commands
         public ManifestFilterOptions FilterOptions { get; set; } = new();
 
         public string? OutputVariableName { get; set; }
+        public string? ImageInfoPath { get; set; }
     }
 
     public class PullImagesOptionsBuilder : ManifestOptionsBuilder
@@ -27,7 +28,9 @@ namespace Microsoft.DotNet.ImageBuilder.Commands
                 .Concat(new Option[]
                 {
                     CreateOption<string>("output-var", nameof(PullImagesOptions.OutputVariableName),
-                        "Azure DevOps variable name to use for outputting the list of pulled image tags")
+                        "Azure DevOps variable name to use for outputting the list of pulled image tags"),
+                    CreateOption<string>("image-info", nameof(PullImagesOptions.ImageInfoPath),
+                        "Path to the image info file describing which images are to be pulled")
                 });
 
         public override IEnumerable<Argument> GetCliArguments() =>

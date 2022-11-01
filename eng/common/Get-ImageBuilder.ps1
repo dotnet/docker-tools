52f4592d49c32289efe0c3b56a4b3465ef657e57
Matt Thalman <mthalman@microsoft.com>
2022-11-01 13:46:29 -0500

    Fix YAML parsing in Get-ImageBuilder.ps1

--- eng/common/Get-ImageBuilder.ps1
+++ eng/common/Get-ImageBuilder.ps1
@@ -2,7 +2,7 @@
 
 # Load common image names
 Get-Content $PSScriptRoot/templates/variables/docker-images.yml |
-Where-Object { $_.Trim() -notlike 'variables:' } |
+Where-Object { $_.Trim().Length -gt 0 -and $_.Trim() -notlike 'variables:' -and $_.Trim() -notlike '# *' } |
 ForEach-Object { 
     $parts = $_.Split(':', 2)
     Set-Variable -Name $parts[0].Trim() -Value $parts[1].Trim() -Scope Global

467bb013f054683cbdc46c5f6ce29c5a5b1ba596
Matt Thalman <mthalman@microsoft.com>
2024-04-26 13:55:08 -0500

    Remove dependency on Azure Fluent package

---
+++ src/Microsoft.DotNet.ImageBuilder/src/BasicAuthenticationCredentials.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.DotNet.ImageBuilder;
+
+#nullable enable
+public class BasicAuthenticationCredentials(string userName, string password)
+{
+    public string UserName { get; } = userName;
+    public string Password { get; } = password;
+}
+#nullable disable

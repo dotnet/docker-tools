c544cb96265b63d104923d185514632f2cbecbf5
Matt Thalman <mthalman@microsoft.com>
2024-05-08 10:02:12 -0500

    Add missing export for IContainerRegistryContentClientFactory (#1274)

--- src/Microsoft.DotNet.ImageBuilder/src/ContainerRegistryContentClientFactory.cs
+++ src/Microsoft.DotNet.ImageBuilder/src/ContainerRegistryContentClientFactory.cs
@@ -1,12 +1,14 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.ComponentModel.Composition;
 using Azure.Containers.ContainerRegistry;
 using Azure.Core;
 
 namespace Microsoft.DotNet.ImageBuilder;
 
 #nullable enable
+[Export(typeof(IContainerRegistryContentClientFactory))]
 internal class ContainerRegistryContentClientFactory : IContainerRegistryContentClientFactory
 {
     public IContainerRegistryContentClient Create(string acrName, string repositoryName, TokenCredential credential) =>

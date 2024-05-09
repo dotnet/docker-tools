8dd545ef29e00a1660c646087d06d6a3eede57b5
Matt Thalman <mthalman@microsoft.com>
2024-05-09 14:29:46 -0500

    Remove Microsoft.IdentityModel.Clients.ActiveDirectory dependency

--- src/Microsoft.DotNet.ImageBuilder/src/AuthHelper.cs
+++ src/Microsoft.DotNet.ImageBuilder/src/AuthHelper.cs
@@ -5,20 +5,11 @@
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace Microsoft.DotNet.ImageBuilder
 {
     public static class AuthHelper
     {
-        public static async Task<string> GetAadAccessTokenAsync(string resource, string tenant, string username, string password)
-        {
-            AuthenticationContext authContext = new AuthenticationContext($"https://login.microsoftonline.com/{tenant}");
-            AuthenticationResult result = await authContext.AcquireTokenAsync(
-                resource, new ClientCredential(username, password));
-            return result.AccessToken;
-        }
-
         public static async Task<string> GetDefaultAccessTokenAsync(string resource)
         {
             DefaultAzureCredential credential  = new();

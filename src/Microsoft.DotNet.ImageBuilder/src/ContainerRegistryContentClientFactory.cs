74261ac842a72d47ffd658080e12e539fbb8f1dd
Matt Thalman <mthalman@microsoft.com>
2024-05-28 12:08:54 -0500

    Workaround for expired token

--- src/Microsoft.DotNet.ImageBuilder/src/ContainerRegistryContentClientFactory.cs
+++ src/Microsoft.DotNet.ImageBuilder/src/ContainerRegistryContentClientFactory.cs
@@ -2,8 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.ComponentModel.Composition;
+using System.Threading;
+using System.Threading.Tasks;
 using Azure.Containers.ContainerRegistry;
 using Azure.Core;
+using Azure.Identity;
 
 namespace Microsoft.DotNet.ImageBuilder;
 
@@ -11,6 +14,22 @@ namespace Microsoft.DotNet.ImageBuilder;
 [Export(typeof(IContainerRegistryContentClientFactory))]
 internal class ContainerRegistryContentClientFactory : IContainerRegistryContentClientFactory
 {
-    public IContainerRegistryContentClient Create(string acrName, string repositoryName, TokenCredential credential) =>
-        new ContainerRegistryContentClientWrapper(new ContainerRegistryContentClient(DockerHelper.GetAcrUri(acrName), repositoryName, credential));
+    private readonly CachedTokenCredential _credential;
+
+    public ContainerRegistryContentClientFactory()
+    {
+        AccessToken token = new DefaultAzureCredential().GetToken(new TokenRequestContext(["https://containerregistry.azure.net/.default"]), CancellationToken.None);
+        _credential = new CachedTokenCredential(token);
+    }
+
+    public IContainerRegistryContentClient Create(string acrName, string repositoryName, TokenCredential credential)
+    {
+        return new ContainerRegistryContentClientWrapper(new ContainerRegistryContentClient(DockerHelper.GetAcrUri(acrName), repositoryName, _credential));
+    }
+
+    private class CachedTokenCredential(AccessToken accessToken) : TokenCredential
+    {
+        public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken) => accessToken;
+        public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken) => ValueTask.FromResult(accessToken);
+    }
 }

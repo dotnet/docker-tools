1a6cfdd3919afce06660da3b26f81ccf29981cca
Matt Thalman <mthalman@microsoft.com>
2024-09-11 14:54:57 -0500

    publishImageInfo: output original and updated content of image info

--- src/Microsoft.DotNet.ImageBuilder/src/Commands/PublishImageInfoOptions.cs
+++ src/Microsoft.DotNet.ImageBuilder/src/Commands/PublishImageInfoOptions.cs
@@ -4,7 +4,7 @@
 
 using System.Collections.Generic;
 using System.CommandLine;
-using System.Linq;
+using static Microsoft.DotNet.ImageBuilder.Commands.CliHelper;
 
 #nullable enable
 namespace Microsoft.DotNet.ImageBuilder.Commands
@@ -12,6 +12,9 @@ namespace Microsoft.DotNet.ImageBuilder.Commands
     public class PublishImageInfoOptions : ImageInfoOptions, IGitOptionsHost
     {
         public GitOptions GitOptions { get; set; } = new GitOptions();
+
+        public string? OriginalImageInfoOutputPath { get; set; }
+        public string? UpdatedImageInfoOutputPath { get; set; }
     }
 
     public class PublishImageInfoOptionsBuilder : ImageInfoOptionsBuilder
@@ -19,12 +22,20 @@ namespace Microsoft.DotNet.ImageBuilder.Commands
         private readonly GitOptionsBuilder _gitOptionsBuilder = GitOptionsBuilder.BuildWithDefaults();
 
         public override IEnumerable<Option> GetCliOptions() =>
-            base.GetCliOptions()
-                .Concat(_gitOptionsBuilder.GetCliOptions());
+            [
+                ..base.GetCliOptions(),
+                .._gitOptionsBuilder.GetCliOptions(),
+                CreateOption<string?>("image-info-orig-path", nameof(PublishImageInfoOptions.OriginalImageInfoOutputPath),
+                    $"Path where the original image info content will be written to"),
+                CreateOption<string?>("image-info-update-path", nameof(PublishImageInfoOptions.UpdatedImageInfoOutputPath),
+                    $"Path where the updated image info content will be written to"),
+
+            ];
 
         public override IEnumerable<Argument> GetCliArguments() =>
-            base.GetCliArguments()
-                .Concat(_gitOptionsBuilder.GetCliArguments());
+            [
+                ..base.GetCliArguments(),
+                .._gitOptionsBuilder.GetCliArguments()
+            ];
     }
 }
-#nullable disable

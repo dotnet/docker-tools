cfb6d6f39f2c2504f0b59d1c3f742be3cc261efa
Logan Bussell <loganbussell@microsoft.com>
2025-02-14 14:23:33 -0800

    Fix ImageBuilder build

--- src/ImageBuilder/build.ps1
+++ src/ImageBuilder/build.ps1
@@ -1,7 +1,7 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
-& ./../../eng/common/build.ps1 `
-    -Manifest src/Microsoft.DotNet.ImageBuilder/manifest.json `
+& $PSScriptRoot/../../eng/common/build.ps1 `
+    -Manifest manifest.json `
     -OptionalImageBuilderArgs "--var UniqueId=$(Get-Date -Format yyyyMMddHHmmss)" `
     -Paths "*"

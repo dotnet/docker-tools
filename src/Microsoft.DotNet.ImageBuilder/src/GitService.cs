0b151fc7b70c94f76af3086ed5eb0e006b58f1b3
Logan Bussell <loganbussell@microsoft.com>
2025-05-07 09:36:51 -0700

    Add logging when cloning repository

--- src/Microsoft.DotNet.ImageBuilder/src/GitService.cs
+++ src/Microsoft.DotNet.ImageBuilder/src/GitService.cs
@@ -2,14 +2,18 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.ComponentModel.Composition;
 using LibGit2Sharp;
 
 namespace Microsoft.DotNet.ImageBuilder
 {
     [Export(typeof(IGitService))]
-    public class GitService : IGitService
+    [method: ImportingConstructor]
+    public class GitService(ILoggerService logger) : IGitService
     {
+        private readonly ILoggerService _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
         public string GetCommitSha(string filePath, bool useFullHash = false)
         {
             return GitHelper.GetCommitSha(filePath, useFullHash);
@@ -17,7 +21,9 @@ namespace Microsoft.DotNet.ImageBuilder
 
         public IRepository CloneRepository(string sourceUrl, string workdirPath, CloneOptions options)
         {
+            _logger.WriteMessage($"Cloning repository {sourceUrl} to {workdirPath}");
             Repository.Clone(sourceUrl, workdirPath, options);
+            _logger.WriteMessage($"Cloned repository {sourceUrl} to {workdirPath}");
             return new Repository(workdirPath);
         }
 

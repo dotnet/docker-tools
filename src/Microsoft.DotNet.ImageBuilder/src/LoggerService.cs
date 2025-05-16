8af2d117df72a8a1682e43b215099e680e05e223
Logan Bussell <loganbussell@microsoft.com>
2025-05-16 14:44:40 -0700

    Add LoggingGroup dependency on ILoggerService and document LoggerService methods

--- src/Microsoft.DotNet.ImageBuilder/src/LoggerService.cs
+++ src/Microsoft.DotNet.ImageBuilder/src/LoggerService.cs
@@ -4,6 +4,7 @@
 
 #nullable enable
 
+using System;
 using System.ComponentModel.Composition;
 
 namespace Microsoft.DotNet.ImageBuilder
@@ -11,39 +12,52 @@ namespace Microsoft.DotNet.ImageBuilder
     [Export(typeof(ILoggerService))]
     internal class LoggerService : ILoggerService
     {
+        /// <inheritdoc />
         public void WriteError(string error)
         {
             Logger.WriteError(error);
         }
 
+        /// <inheritdoc />
         public void WriteHeading(string heading)
         {
             Logger.WriteHeading(heading);
         }
 
+        /// <inheritdoc />
         public void WriteMessage(string? message = null)
         {
             Logger.WriteMessage(message);
         }
 
+        /// <inheritdoc />
         public void WriteSubheading(string subheading)
         {
             Logger.WriteSubheading(subheading);
         }
 
+        /// <inheritdoc />
         public void WriteCommand(string command)
         {
             Logger.WriteCommand(command);
         }
 
+        /// <inheritdoc />
         public void WriteWarning(string message)
         {
             Logger.WriteWarning(message);
         }
 
+        /// <inheritdoc />
         public void WriteDebug(string message)
         {
             Logger.WriteDebug(message);
+        }
+
+        /// <inheiritdoc />
+        public IDisposable LogGroup(string name)
+        {
+            return new LoggingGroup(name, this);
         }
     }
 }

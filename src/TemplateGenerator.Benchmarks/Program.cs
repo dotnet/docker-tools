da4d45a26c9e8d0adfb20124938f7a6c436a7f15
Logan Bussell <loganbussell@microsoft.com>
2025-09-17 22:03:08 -0700

    Add benchmark for round-trip serialization

---
+++ src/TemplateGenerator.Benchmarks/Program.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.DotNet.ImageBuilder.ReadModel;
+
+namespace Microsoft.DotNet.DockerTools.TemplateGenerator;
+
+[MemoryDiagnoser]
+public class Benchmarks
+{
+    private static readonly string s_manifestPath =
+        Environment.GetEnvironmentVariable("MANIFEST_PATH") ?? "manifest.json";
+
+    [Benchmark]
+    public async Task<string> RoundTripManifestSerialization()
+    {
+        ManifestInfo manifest = await ManifestInfo.LoadAsync(s_manifestPath);
+        return manifest.ToJsonString();
+    }
+
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Benchmarks>();
+    }
+}

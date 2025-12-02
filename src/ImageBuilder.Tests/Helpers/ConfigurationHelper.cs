8da1ae6799b6111dffcef10326068426f5cb25d8
Logan Bussell <loganbussell@microsoft.com>
2025-12-02 10:24:43 -0800

    Add param doc comment

--- src/ImageBuilder.Tests/Helpers/ConfigurationHelper.cs
+++ src/ImageBuilder.Tests/Helpers/ConfigurationHelper.cs
@@ -13,6 +13,10 @@ internal static class ConfigurationHelper
     /// <summary>
     /// Sets up a mock of IOptions that returns <paramref name="value"/>.
     /// </summary>
+    /// <param name="value">
+    /// The value to be returned by the IOptions mock.
+    /// If <paramref name="value"/> is null, a new instance of T is created.
+    /// </param>
     public static IOptions<T> CreateOptionsMock<T>(T? value = null) where T : class, new()
     {
         var optionsMock = new Mock<IOptions<T>>();

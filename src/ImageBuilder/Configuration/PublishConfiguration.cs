11d848b033d4b9cc08558bc42734eeeaa37f8b0a
Logan Bussell <loganbussell@microsoft.com>
2025-12-04 10:45:49 -0800

    Add other registries to PublishConfiguration

--- src/ImageBuilder/Configuration/PublishConfiguration.cs
+++ src/ImageBuilder/Configuration/PublishConfiguration.cs
@@ -11,7 +11,26 @@ public sealed record PublishConfiguration
 {
     public static string ConfigurationKey => nameof(PublishConfiguration);
 
+    /// <summary>
+    /// Images are built and pushed into this registry before testing and publishing.
+    /// </summary>
     public AcrConfiguration? BuildAcr { get; set; }
+
+    /// <summary>
+    /// Images are copied from <see cref="BuildAcr"/> to this registry during publishing.
+    /// </summary>
+    public AcrConfiguration? PublishAcr { get; set; }
+
+    /// <summary>
+    /// External image dependencies are mirrored to this registry.
+    /// </summary>
+    public AcrConfiguration? InternalMirrorAcr { get; set; }
+
+    /// <summary>
+    /// External images are mirrored to this registry. This registry has anonymous pull access
+    /// enabled so that it can be used in public PR validation.
+    /// </summary>
+    public AcrConfiguration? PublicMirrorAcr { get; set; }
 
     /// <summary>
     /// Gets all ACR configurations that were provided in the publish configuration.

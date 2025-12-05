c049b1606854f2211de06507dc2253403e6fb6b1
Logan Bussell <loganbussell@microsoft.com>
2025-12-05 12:54:27 -0800

    Simplify IAcrClientFactory to look up service connections internally
    - Remove TokenCredential parameter from Create method
    - Inject IOptions<PublishConfiguration> and IAzureTokenCredentialProvider
    - Use PublishConfiguration.FindAcrByName() to look up service connections
    - Throw if no service connection is found for the requested ACR

--- src/ImageBuilder/AcrClientFactory.cs
+++ src/ImageBuilder/AcrClientFactory.cs
@@ -2,15 +2,35 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using Azure.Containers.ContainerRegistry;
 using Azure.Core;
+using Microsoft.DotNet.ImageBuilder.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace Microsoft.DotNet.ImageBuilder;
 
 #nullable enable
 
-public class AcrClientFactory : IAcrClientFactory
+public class AcrClientFactory(
+    IAzureTokenCredentialProvider tokenCredentialProvider,
+    IOptions<PublishConfiguration> publishConfigOptions
+) : IAcrClientFactory
 {
-    public IAcrClient Create(string acrName, TokenCredential credential) =>
-        new AcrClientWrapper(new ContainerRegistryClient(DockerHelper.GetAcrUri(acrName), credential));
+    private readonly IAzureTokenCredentialProvider _tokenCredentialProvider = tokenCredentialProvider;
+    private readonly PublishConfiguration _publishConfig = publishConfigOptions.Value;
+
+    public IAcrClient Create(string acrName)
+    {
+        var acrConfig = _publishConfig.FindAcrByName(acrName);
+        if (acrConfig?.ServiceConnection is null)
+        {
+            throw new InvalidOperationException(
+                $"No service connection found for ACR '{acrName}'. " +
+                $"Ensure the ACR is configured in the publish configuration with a valid service connection.");
+        }
+
+        TokenCredential credential = _tokenCredentialProvider.GetCredential(acrConfig.ServiceConnection);
+        return new AcrClientWrapper(new ContainerRegistryClient(DockerHelper.GetAcrUri(acrName), credential));
+    }
 }

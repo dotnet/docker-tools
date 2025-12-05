82710a066443918a95ec7a637f83a9457f8ba1de
Logan Bussell <loganbussell@microsoft.com>
2025-12-05 12:54:33 -0800

    Simplify IAcrContentClientFactory to look up service connections internally
    - Change Create method to accept Acr type instead of string acrName
    - Remove IServiceConnection parameter
    - Inject IOptions<PublishConfiguration> to look up service connections
    - Use acr.RegistryUri directly instead of DockerHelper.GetAcrUri()

--- src/ImageBuilder/IAcrContentClientFactory.cs
+++ src/ImageBuilder/IAcrContentClientFactory.cs
@@ -1,14 +1,12 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.DotNet.ImageBuilder.Configuration;
 
 namespace Microsoft.DotNet.ImageBuilder;
 
 #nullable enable
 public interface IAcrContentClientFactory
 {
-    IAcrContentClient Create(
-        string acrName,
-        string repositoryName,
-        IServiceConnection? serviceConnection);
+    IAcrContentClient Create(Acr acr, string repositoryName);
 }

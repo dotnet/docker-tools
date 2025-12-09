b5fc9c182db24d624443a8691e7de1c59399a6b4
Logan Bussell <loganbussell@microsoft.com>
2025-12-09 13:48:46 -0800

    Explain why name property is needed

--- src/ImageBuilder/Configuration/ServiceConnection.cs
+++ src/ImageBuilder/Configuration/ServiceConnection.cs
@@ -7,6 +7,10 @@ namespace Microsoft.DotNet.ImageBuilder.Configuration;
 
 public record ServiceConnection : IServiceConnection
 {
+    // The Name property is not used in ImageBuilder, but it is used in the Azure Pipeline templates.
+    // See eng/pipelines/templates/stages/build-test-publish.yml for an example.
+    // This code should serve as the source of truth for the service connection and publish configuration schemas,
+    // so the property still needs to be included here.
     /// <inheritdoc/>
     public string Name { get; set; } = "";
 
